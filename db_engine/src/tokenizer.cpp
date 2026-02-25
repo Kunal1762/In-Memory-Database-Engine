1b208cac828e46a630754ccded6a024d28fa7086
Kunal1762 <kunalbari098@gmail.com>
2026-02-25 19:25:40 +0530

    added member functions in tokenizer

--- db_engine/src/tokenizer.cpp
+++ db_engine/src/tokenizer.cpp
@@ -1,8 +1,8 @@
 #include "../include/tokenizer.h"
-#include<unordered_map>
-#include<cctype>
+#include <unordered_map>
+#include <cctype>
 
-static std::unordered_map<std::string,TokenType>Keywords={
+static std::unordered_map<std::string, TokenType> Keywords = {
     {"CREATE", TokenType::CREATE},
     {"TABLE", TokenType::TABLE},
     {"INSERT", TokenType::INSERT},
@@ -15,5 +15,32 @@ static std::unordered_map<std::string,TokenType>Keywords={
     {"INT", TokenType::INT_TYPE},
     {"STRING", TokenType::STRING_TYPE},
     {"BOOL", TokenType::BOOL_TYPE},
-    {"DOUBLE", TokenType::DOUBLE_TYPE}
-};
+    {"DOUBLE", TokenType::DOUBLE_TYPE}};
+
+Tokenizer::Tokenizer(const std::string &in)
+: input(in), pos(0) {};
+
+bool Tokenizer::isAtEnd() const{
+    return pos>=input.size();
+}
+
+char Tokenizer::currentChar() const{
+    if(isAtEnd()) return '\0';
+    return input[pos];
+}
+void Tokenizer::skipWhiteSpaces(){
+    while(!isAtEnd() && std::isspace(currentChar())){
+        pos++;
+    }
+}
+
+Token Tokenizer::peek(){
+    size_t savedPos=pos;
+    Token t=next();
+    pos=savedPos;
+    return t;
+}
+
+Token Tokenizer::next(){
+
+}
